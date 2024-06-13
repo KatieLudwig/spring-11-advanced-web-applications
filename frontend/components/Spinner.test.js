5ebe265edacf79595b615df05e9298a4e4ad92a2
Katie Ludwig <katierludwig@gmail.com>
2024-06-13 14:12:25 -0700

    finished!

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,20 +1,22 @@
-//Spinner.test.js
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
-// test('sanity', () => {
-//   expect(true).toBe(false)
-// })
+import React from'react'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import Spinner from './Spinner'
 
-// import React from'react'
-// import { render } from '@testing-library/react'
-// import Spinner from './Spinner'
+test('sanity', () => {
+  expect(true).toBe(true)
+})
 
-// test('renders a Spinner component when on is true', () => {
-//   const { getByText } = render(<Spinner on={true} />)
-//   expect(getbyText(/Please wait/i)).toBeInTheDocument()
-// })
-
-// test('does not render Spinner component when on is false', () => {
-//   const { queryByText } = render(<Spinner on={false} />)
-//   expect(queryByText(/Please wait/i)).not.toBeInTheDocument()
-// })
+test('renders nothing when `on` is false', () => {
+    render(<Spinner on={false} />)
+    const spinnerElement = screen.queryByText(/Please wait.../i)
+    expect(spinnerElement).not.toBeInTheDocument()
+  })
+  
+  test('renders loading text when `on` is true', () => {
+    render(<Spinner on={true} />)
+    const spinnerElement = screen.getByText(/Please wait.../i)
+    expect(spinnerElement).toBeInTheDocument()
+  })
